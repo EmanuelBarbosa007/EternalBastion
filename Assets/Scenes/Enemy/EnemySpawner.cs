166b35a1277735df606ecee7124f26b2de18db3f
GabrielSavignon <a27924@alunos.ipca.pt>
2025-10-12 22:21:43 +0100

    Botei o cavalo, os diferentes tipos de inimigos (cavalo e tanque), cavalo nasce por ultimo e é mais rapido, tanque nasce primeiro e tem mais vida

--- Assets/Scenes/Enemy/EnemySpawner.cs
+++ Assets/Scenes/Enemy/EnemySpawner.cs
@@ -1,6 +1,6 @@
 using System.Collections;
 using UnityEngine;
-
+/*
 public class EnemySpawner : MonoBehaviour
 {
     public GameObject enemyPrefab;
@@ -23,6 +23,50 @@ public class EnemySpawner : MonoBehaviour
                 Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
                 yield return new WaitForSeconds(spawnInterval);
             }
+            yield return new WaitForSeconds(timeBetweenWaves);
+        }
+    }
+} */
+using System.Collections;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public GameObject firstEnemyPrefab;
+    public GameObject lastEnemyPrefab;
+
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 20f;
+    public int enemiesPerWave = 5;
+    public float spawnInterval = 1f;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        while (true)
+        {
+            for (int i = 0; i < enemiesPerWave; i++)
+            {
+                GameObject prefab;
+
+                // primeiro da wave
+                if (i == 0 && firstEnemyPrefab != null)
+                    prefab = firstEnemyPrefab;
+                // último da wave
+                else if (i == enemiesPerWave - 1 && lastEnemyPrefab != null)
+                    prefab = lastEnemyPrefab;
+                else
+                    prefab = enemyPrefab;
+
+                Instantiate(prefab, spawnPoint.position, spawnPoint.rotation);
+                yield return new WaitForSeconds(spawnInterval);
+            }
+
             yield return new WaitForSeconds(timeBetweenWaves);
         }
     }
